678735e7ccfabe3b55c609e9432fc9433d695c19
Saderfing <saderfing@gmail.com>
2022-06-02 12:46:50 +0200

    livid but better

--- boss.py
+++ boss.py
@@ -1,14 +1,17 @@
 from entity import Entity, Player
 import pygame
+from random import randint
 
-class Schwamm(Entity):
+class Livid(Entity):
     def __init__(self, pos:list) -> None:
-        GFX = pygame.image.load('assets/Schwamm.png').convert_alpha()
+        GFX = pygame.image.load('assets/livid.png').convert_alpha()
         HP, DF, SP, DMG = 1000, 50, 10, 100
         
         super().__init__(pos, GFX, HP, DF, SP, DMG)
         
-        self.phases = {}
+        self.phases = {0:self.shadow_dupes}
+        self.current_phase = 0
+        self.shadows = []
         
     def update(self):
         pass
@@ -16,9 +19,17 @@ class Schwamm(Entity):
     def pathfinding(self):
         pass
     
-    def water_splash(self):
+    def shadow_daggers(self):
         pass
     
+    def shadow_dupes(self):
+        for i in range(4):
+            self.shadows.append(Shadows((self.pos[0] + randint(-10, 10), self.pos[1] + randint(-10, 10))))
+    
+class Shadows(Livid):
+    def __init__(self, pos: list) -> None:
+        super().__init__(pos)
+
     
 if __name__ == '__main__':
     #pygame.init()
@@ -27,7 +38,7 @@ if __name__ == '__main__':
     HEIGHT = 400
     win = pygame.display.set_mode((WIDTH, HEIGHT))
     player = Player([0, 0], 100, 50, 10, 10, [])
-    boss = Schwamm([0,0])
+    boss = Livid([WIDTH//2,HEIGHT//2])
 
     clock = pygame.time.Clock()
     FPS = 60
@@ -35,11 +46,14 @@ if __name__ == '__main__':
         clock.tick(FPS)
 
         win.fill((0,0,0))
-        player.update([WIDTH, HEIGHT], [0, 0])
+        player.update([WIDTH, HEIGHT])
+        boss.update()
         win.blit(player.GFX, player.pos)
         win.blit(boss.GFX, boss.pos)
         for arrow in player.shot_arrows:
             win.blit(arrow.GFX, arrow.pos)
+        for shadow in boss.shadows:
+            win.blit(shadow.GFX, shadow.pos)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
